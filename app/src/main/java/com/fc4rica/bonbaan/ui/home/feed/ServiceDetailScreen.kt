4154f33c61f5df3cf50cb25df351f2315ddfa2e5
FC4RICA <kantinan.fw@gmail.com>
2025-03-03 19:31:13 +0700

    Refactor: Add nested navigation for Feed
    - Created `SearchScreen` and `ServiceDetailScreen` in the `feed` sub-package.
    - Added nested navigation for `SearchScreen` and `ServiceDetailScreen` within the Feed section.
    - Modified `Screen` to define nested screen routes.
    - Updated `HomeScreen` to include the bottom bar only for bottom navigation routes.
    - Updated `FeedScreen` to have a button that navigates to `ServiceDetailScreen`.

---
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/feed/ServiceDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.fc4rica.bonbaan.ui.home.feed
+
+import androidx.compose.runtime.Composable
+import com.fc4rica.bonbaan.ui.MockScreen
+
+@Composable
+fun ServiceDetailScreen (serviceId: String) {
+    MockScreen("SERVICE DETAIL $serviceId")
+}
