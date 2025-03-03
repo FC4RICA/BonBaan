73ec61ed258e0b44d7ed4a1eba353d24834dc77d
FC4RICA <kantinan.fw@gmail.com>
2025-03-03 19:31:13 +0700

    Refactor: Add nested navigation for Feed
    - Created `SearchScreen` and `ServiceDetailScreen` in the `feed` sub-package.
    - Added nested navigation for `SearchScreen` and `ServiceDetailScreen` within the Feed section.
    - Modified `Screen` to define nested screen routes.
    - Updated `HomeScreen` to include the bottom bar only for bottom navigation routes.
    - Updated `FeedScreen` to have a button that navigates to `ServiceDetailScreen`.

--- app/src/main/java/com/fc4rica/bonbaan/ui/navigation/Screen.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/navigation/Screen.kt
@@ -16,14 +16,14 @@ sealed class Screen(val route: String) {
     data object Notification : Screen("notification")
     data object Profile : Screen("profile")
 
-//    // Nested Screens (Inside Feed)
-//    data object Search : Screen("search")
-//    data class ServiceDetail(val serviceId: String) : Screen("serviceDetail/{serviceId}") {
-//        companion object {
-//            fun createRoute(serviceId: String) = "serviceDetail/$serviceId"
-//        }
-//    }
-//
+    // Nested Screens (Inside Feed)
+    data object Search : Screen("search")
+    data class ServiceDetail(val serviceId: String) : Screen("serviceDetail/{serviceId}") {
+        companion object {
+            fun createRoute(serviceId: String) = "serviceDetail/$serviceId"
+        }
+    }
+
 //    // Nested Screens (Inside Profile)
 //    data object ServicesStatus : Screen("servicesStatus")
 //    data object PreviousServices : Screen("previousServices")
@@ -33,6 +33,5 @@ sealed class Screen(val route: String) {
 //            fun createRoute(previousServiceId: String) = "previousServiceDetail/$previousServiceId"
 //        }
 //    }
-//
-//    data object ProfileSetting : Screen("profileSetting")
+//    data object ProfileSetting : Screen("profileSSetting")
 }
