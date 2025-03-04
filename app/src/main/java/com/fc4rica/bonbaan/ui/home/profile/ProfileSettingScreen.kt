b8d7833ef9141380d9faf625ce52e09118d7a64d
FC4RICA <kantinan.fw@gmail.com>
2025-03-04 12:52:42 +0700

    Feat: Implement nested profile screens and navigation
    - Added `OrdersStatusScreen`, `OrderStatusDetailScreen`, `PreviousVowsScreen`, `PreviousVowDetailScreen`, and `ProfileSettingScreen` for the profile section.
    - Defined new navigation routes for these screens in `Screen.kt`.
    - Implemented navigation to these nested profile screens within the `Home` composable, including handling dynamic parameters.
    - Added new screens' composables in relevant package.

---
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/profile/ProfileSettingScreen.kt
@@ -0,0 +1,9 @@
+package com.fc4rica.bonbaan.ui.home.profile
+
+import androidx.compose.runtime.Composable
+import com.fc4rica.bonbaan.ui.MockScreen
+
+@Composable
+fun ProfileSettingScreen() {
+    MockScreen("PROFILE SETTING")
+}
