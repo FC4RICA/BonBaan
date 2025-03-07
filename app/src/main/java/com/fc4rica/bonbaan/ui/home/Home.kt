7e1faa6514436ddea27df3649106373259a2e31c
FC4RICA <kantinan.fw@gmail.com>
2025-03-07 19:20:48 +0700

    Refactor: Implement static color scheme and update home screen UI

--- app/src/main/java/com/fc4rica/bonbaan/ui/home/Home.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/Home.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -164,4 +165,10 @@ fun BottomNavBar(
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
