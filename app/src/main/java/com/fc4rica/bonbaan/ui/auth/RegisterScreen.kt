1d90fffab841bf55082a895efe72b94922ae10bd
Thanachot-W <thanachot.wongy@mail.kmutt.ac.th>
2025-03-08 13:10:05 +0700

    register

--- app/src/main/java/com/fc4rica/bonbaan/ui/auth/RegisterScreen.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/auth/RegisterScreen.kt
@@ -2,8 +2,31 @@ package com.fc4rica.bonbaan.ui.auth
 
 import androidx.compose.runtime.Composable
 import com.fc4rica.bonbaan.ui.MockScreen
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.bonbaan.ui.components.ButtonComponent
+import com.example.bonbaan.ui.components.TextFieldComponent
 
 @Composable
 fun RegisterScreen() {
-    MockScreen("REGISTER")
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextFieldComponent(label = "อีเมลล์", value = email, onValueChange = { email = it })
+        Spacer(modifier = Modifier.height(10.dp))
+        TextFieldComponent(label = "รหัสผ่าน", value = password, onValueChange = { password = it })
+        Spacer(modifier = Modifier.height(10.dp))
+        TextFieldComponent(label = "ยืนยันรหัสผ่าน", value = confirmPassword, onValueChange = { confirmPassword = it })
+        Spacer(modifier = Modifier.height(20.dp))
+        ButtonComponent(text = "สมัครสมาชิก", onClick = { /* TODO: Handle Register */ })
+    }
 }
---
+++ app/src/main/java/component/TextField.kt
@@ -0,0 +1,28 @@
+package com.fc4rica.bonbaan.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun TextFieldComponent(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = keyboardType),
+        visualTransformation = visualTransformation,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
