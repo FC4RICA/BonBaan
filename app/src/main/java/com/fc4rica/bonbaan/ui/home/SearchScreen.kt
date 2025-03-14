449e168707fc8c2abc071ca3aa66eb94d56a9eb7
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-03-14 21:01:39 +0700

    Feat: SearchScreen

---
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/SearchScreen.kt
@@ -0,0 +1,148 @@
+package com.fc4rica.bonbaan.ui.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.fc4rica.bonbaan.ui.components.BonBaanTextField
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SearchScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEBEBEB))
+            ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Searchbar()
+        SearchHistory()
+        Recommend()
+    }
+
+}
+
+@Composable
+fun Searchbar(){
+    Box(
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .background(Color(0xFF5E17EB)),
+    contentAlignment = Alignment.Center
+) {
+        Row( modifier = Modifier.padding(horizontal = 16.dp),verticalAlignment = Alignment.CenterVertically){
+            Text(text = "Back")
+            Spacer(modifier = Modifier.width(16.dp))
+            TextField(
+                value = "",
+                onValueChange = {},
+
+                placeholder = { Text("ค้นหา") },
+                modifier = Modifier.background(Color(0xFF5E17EB))
+
+                    .height(38.dp)
+                    .fillMaxWidth(0.9f)
+                    .clip(
+                        RoundedCornerShape(7.dp),
+
+
+                        ))
+        }
+
+}
+}
+
+@Composable
+fun SearchHistory(){
+    Column(modifier = Modifier
+        .padding(top = 12.dp)
+        .fillMaxWidth()
+        .background(color = Color.White)) {
+        Text(text = "ประวัติการค้นหา", modifier = Modifier.padding(16.dp), fontSize = 17.sp,color = Color(0xFF5E17EB),fontWeight = FontWeight.Bold)
+        HistoryItem("วัดฟ้าประทาน")
+        HistoryItem("วัดดอยคำ ยำอาฟเตอร์ยู")
+        HistoryItem("วัดดูยูมีน ไอดอนโน บัดไอเลิฟยู")
+    }
+
+}
+
+@Composable
+fun HistoryItem(Name: String){
+    Column (modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp)){
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            , horizontalArrangement = Arrangement.SpaceBetween){Text(text = "$Name")
+            Text(text = "Delete")}
+
+
+    }
+}
+
+@Composable
+fun Recommend(){
+    Column(modifier = Modifier
+        .padding(top = 12.dp, bottom = 12.dp)
+        .fillMaxWidth()
+        .background(color = Color.White)) {
+        Text(text = "แนะนำ", modifier = Modifier.padding(16.dp), fontSize = 17.sp,color = Color(0xFF5E17EB),fontWeight = FontWeight.Bold)
+        RecommendItem("วัดฟ้าประทาน")
+        RecommendItem("วัดดอยคำ ยำอาฟเตอร์ยู")
+        RecommendItem("วัดดูยูมีน ไอดอนโน บัดไอเลิฟยู")
+    }
+}
+
+@Composable
+fun RecommendItem(Name: String){
+    Column (modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp).fillMaxWidth().background(Color(0xFFEBEBEB))){
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            , horizontalArrangement = Arrangement.SpaceBetween){
+            Column {
+                Text(text = "$Name")
+                Text(text = "Location")
+                Text(
+                    text = "หมวดหมู่",
+                    modifier = Modifier.background(Color(0xFF5E17EB)),
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            }
+
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+    SearchScreen()
+}
