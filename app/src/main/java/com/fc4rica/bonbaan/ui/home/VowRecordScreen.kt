ad81804ef6d32272ebea613d37549efaccec2748
FC4RICA <kantinan.fw@gmail.com>
2025-03-16 16:59:24 +0700

    Refactor: Update package and rename VowRecordScreen
    - Moved `VowRecordScreen` to the `ui.home` package.
    - Removed the redundant `SearchScreen` function.

--- app/src/main/java/com/fc4rica/bonbaan/ui/home/VowRecordScreen.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/VowRecordScreen.kt
@@ -1,14 +1,9 @@
-package com.fc4rica.bonbaan.ui.home.feed
+package com.fc4rica.bonbaan.ui.home
 
 import androidx.compose.runtime.Composable
 import com.fc4rica.bonbaan.ui.MockScreen
 
 @Composable
-<<<<<<<< HEAD:app/src/main/java/com/fc4rica/bonbaan/ui/home/VowRecordScreen.kt
 fun VowRecordScreen() {
     MockScreen("RECORDS")
-========
-fun SearchScreen() {
-    MockScreen("SEARCH")
->>>>>>>> 3b1afeb38487a14c672313a4b47ede52770548c6:app/src/main/java/com/fc4rica/bonbaan/ui/home/feed/SearchScreen.kt
 }
