589240dc7ba51dcc963b6e20bcc49017966f51d9
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-03-17 11:59:40 +0700

    Refactor : Add Icon,BonBaan Component to Homepage

--- app/src/main/java/com/fc4rica/bonbaan/ui/home/Homepage.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/Homepage.kt
@@ -1,5 +1,6 @@
 package com.fc4rica.bonbaan.ui.home
 
+import android.icu.text.StringSearch
 import com.fc4rica.bonbaan.R
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
@@ -16,7 +17,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -35,13 +38,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.shadow
+import com.fc4rica.bonbaan.ui.components.BonBaanTextField
 
 
 @Composable
 fun Homepage() {
+    var searchValue by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxSize().background(Color(0xFFEBEBEB))) {
-        SearchBar()
+    Column(modifier = Modifier.fillMaxSize()
+        .background(Color(0xFFEBEBEB))) {
+        SearchBar(searchValue,onValueChange = { searchValue = it })
         Spacer(modifier = Modifier.height(12.dp))
         Category()
         Spacer(modifier = Modifier.height(12.dp))
@@ -52,7 +63,9 @@ fun Homepage() {
 }
 
 @Composable
-fun SearchBar() {
+fun SearchBar(searchValue: String, onValueChange: (String) -> Unit) {
+
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -60,19 +73,20 @@ fun SearchBar() {
             .background(Color(0xFF5E17EB)),
         contentAlignment = Alignment.Center
     ) {
-        TextField(
-            value = "",
-            onValueChange = {},
-
-            placeholder = { Text("ค้นหา") },
-            modifier = Modifier.background(Color(0xFF5E17EB))
-
-                .height(38.dp)
-                .fillMaxWidth(0.9f)
-                .clip(RoundedCornerShape(7.dp),
-
-
-                ))
+        BonBaanTextField(label = "ค้นหา", value = searchValue, onValueChange = onValueChange)
+//        TextField(
+//            value = "",
+//            onValueChange = {},
+//
+//            placeholder = { Text("ค้นหา") },
+//            modifier = Modifier.background(Color(0xFF5E17EB))
+//
+//                .height(38.dp)
+//                .fillMaxWidth(0.9f)
+//                .clip(RoundedCornerShape(7.dp),
+//
+//
+//                ))
 
     }
 }
@@ -99,11 +113,11 @@ fun Recommended(){
     Column (modifier = Modifier.fillMaxWidth().background(color = Color.White)){
         Text(text = "แนะนำ",modifier = Modifier.padding(8.dp), fontSize = 17.sp,color = Color(0xFF5E17EB),fontWeight = FontWeight.Bold)
         LazyVerticalGrid(
-            columns = GridCells.Fixed(2), // ✅ แถวละ 2 คอลัมน์
+            columns = GridCells.Fixed(2),
             modifier = Modifier.fillMaxWidth(),
             contentPadding = PaddingValues(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp), // ✅ ระยะห่างแนวนอน
-            verticalArrangement = Arrangement.spacedBy(8.dp) // ✅ ระยะห่างแนวตั้ง
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(items) { item ->
                 RecommendationCard(title = item)
@@ -116,7 +130,7 @@ fun Recommended(){
 @Composable
 fun SubCategory(name: String){
     Column(modifier = Modifier.padding(top= 12.dp, start = 22.dp,end = 22.dp, bottom = 12.dp),horizontalAlignment = Alignment.CenterHorizontally){
-        Box(modifier = Modifier.height(24.dp).width(24.dp).background(Color(0xFF5E17EB))){}
+        Icon(Icons.Filled.Work, contentDescription = "Briefcase", tint = Color(0xFF5E17EB))
         Text(text = name)  }
 }
 
@@ -129,23 +143,20 @@ fun RecommendationCard(title: String) {
             .width(220.dp)
             .fillMaxWidth()
             .padding(8.dp)
-            .background(color = Color.White)
             .clip(RoundedCornerShape(12.dp)),
-        elevation = CardDefaults.cardElevation(4.dp)
+
     ) {
-        Column {
+        Column(modifier = Modifier
+            .background(Color(0xFFF9F9F9))) {
 
             Box(modifier = Modifier.fillMaxWidth()) {
                 Image(
-                    painter = painterResource(id = R.drawable.temple),
+                    painter = painterResource(id = R.drawable.logo1),
                     contentDescription = "Logo",
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(130.dp)
-
                 )
-
-
                 Box(
                     modifier = Modifier
                         .background(Color(0xFF5E17EB), shape = RoundedCornerShape(8.dp))
