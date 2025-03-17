3dacae5aa3d343849a7baec5bd6f2cb2a1c320e5
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-03-17 11:02:04 +0700

    Refactor : Add BonBaan Component to LoginScreen

--- app/src/main/java/com/fc4rica/bonbaan/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/auth/LoginScreen.kt
@@ -1,18 +1,20 @@
 package com.fc4rica.bonbaan.ui.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import com.fc4rica.bonbaan.ui.components.ButtonVariant
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,55 +23,61 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.style.TextDecoration
+import com.fc4rica.bonbaan.R
+import com.fc4rica.bonbaan.ui.components.BonBaanButton
+import com.fc4rica.bonbaan.ui.components.BonBaanTextField
+
 
 
 @Composable
-fun LoginScreen(name: String) {
+fun LoginScreen() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var result by remember { mutableStateOf("") }
 
     Box(modifier = Modifier.fillMaxSize().background(Color(0xFF5E17EB)), contentAlignment = Alignment.Center){
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
 
-
-            Spacer(modifier = Modifier.height(20.dp))
-//            Text(text = "Hello $name!")
-            Text(text="เข้าสู่ระบบ", fontSize = 24.sp,color = Color.White,fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(20.dp))
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(text ="อีเมลล์",color = Color.White, modifier = Modifier.align(Alignment.Start))
-            Spacer(modifier = Modifier.height(12.dp))
-            TextField(
-                modifier = Modifier.height(25.dp),
-                value = email,
-                onValueChange = { email = it }
+            Image(
+                painter = painterResource(id = R.drawable.logo2),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(8.dp)
             )
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(text="เข้าสู่ระบบ", fontSize = 24.sp,color = Color.White,fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(text ="อีเมล",color = Color.White, modifier = Modifier.align(Alignment.Start))
+            Spacer(modifier = Modifier.height(12.dp))
+            BonBaanTextField(label = "อีเมล", value = email, onValueChange = { email = it })
+
 
             Spacer(modifier = Modifier.height(16.dp))
             Text(text ="รหัสผ่าน",color = Color.White, modifier = Modifier.align(Alignment.Start))
             Spacer(modifier = Modifier.height(12.dp))
-            TextField(
-                modifier = Modifier.height(25.dp),
-                value = password,
-                onValueChange = { password = it }
-            )
-            Spacer(modifier = Modifier.height(35.dp))
-            Button(
-                onClick = { result = email },shape = RoundedCornerShape(7.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+            BonBaanTextField(label = "รหัสผ่าน", value = password, onValueChange = {password = it})
 
+            Spacer(modifier = Modifier.height(35.dp))
+
+            BonBaanButton(
+                text = "เข้าสู่ระบบ",
+                onClick = { },
                 modifier = Modifier
                     .height(55.dp)
-                    .width(280.dp)
-            ) {
-                Text("เข้าสู่ระบบ",color = Color(0xFF5E17EB),fontWeight = FontWeight.Bold)
-            }
+                    .fillMaxWidth()
+
+            )
             Spacer(modifier = Modifier.height(40.dp))
             Row{
                 Text(text="ยังไม่มีบัญชีผู้ใช้? ",color = Color.White) //ไม่รู้จะทำยังไงให้มันเว้น
@@ -77,11 +85,7 @@ fun LoginScreen(name: String) {
                 Text(text="สมัครเลย",color = Color.Yellow,textDecoration = TextDecoration.Underline)
             }
 
-
-
         }
-
-
 
     }
 
@@ -89,5 +93,5 @@ fun LoginScreen(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun LoginPagePreview() {
-    LoginScreen("สาวดนตรี")
+    LoginScreen()
 }
