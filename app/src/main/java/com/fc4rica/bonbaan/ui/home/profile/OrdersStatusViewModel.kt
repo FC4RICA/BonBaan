de76990ba1c153bb666ef5e498b103a2644b114a
FC4RICA <kantinan.fw@gmail.com>
2025-04-10 05:24:21 +0700

    Feat: Implement fetching of order statuses and combine loading states
    - Implemented fetching of order statuses from the repository.
    - Updated the UI state to include a list of statuses and a selected status ID.
    - Used `combine` from `kotlinx.coroutines.flow` to combine the loading states of orders and statuses, updating the overall loading state accordingly.

--- app/src/main/java/com/fc4rica/bonbaan/ui/home/profile/OrdersStatusViewModel.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/profile/OrdersStatusViewModel.kt
@@ -3,14 +3,18 @@ package com.fc4rica.bonbaan.ui.home.profile
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fc4rica.bonbaan.domain.model.Order
+import com.fc4rica.bonbaan.domain.model.Status
 import com.fc4rica.bonbaan.domain.repository.OrderRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 data class OrdersStatusUiState(
     val orders: List<Order> = emptyList(),
+    val status: List<Status> = emptyList(),
+    val selectedStatusId: String? = null,
     val errorMessage: String? = null,
     val isLoading: Boolean = false
 )
@@ -22,12 +26,24 @@ class OrdersStatusViewModel(
     private val _state = MutableStateFlow(OrdersStatusUiState())
     val state = _state.asStateFlow()
 
+    private val _isOrderLoading = MutableStateFlow(false)
+    private val _isStatusLoading = MutableStateFlow(false)
+
     init {
         getOrders()
+        getStatuses()
+
+        viewModelScope.launch {
+            combine(_isOrderLoading, _isStatusLoading) { profileLoading, serviceLoading ->
+                profileLoading || serviceLoading
+            }.collect { isLoading ->
+                _state.update { it.copy(isLoading = isLoading) }
+            }
+        }
     }
 
     private fun getOrders() {
-        _state.update { it.copy(isLoading = true) }
+        _isOrderLoading.value = true
         viewModelScope.launch {
             val result = orderRepository.getOrders()
             result.fold(
@@ -39,6 +55,23 @@ class OrdersStatusViewModel(
                     _state.update { it.copy(errorMessage = error.message, isLoading = false) }
                 }
             )
+            _isOrderLoading.value = false
+        }
+    }
+
+    private fun getStatuses() {
+        _isStatusLoading.value = true
+        viewModelScope.launch {
+            val result = orderRepository.getOrderStatuses()
+            result.fold(
+                onSuccess = { statuses ->
+                    _state.update { it.copy(status = statuses, isLoading = false) }
+                },
+                onFailure = { error ->
+                    _state.update { it.copy(errorMessage = error.message, isLoading = false) }
+                }
+            )
+            _isStatusLoading.value = false
         }
     }
 
