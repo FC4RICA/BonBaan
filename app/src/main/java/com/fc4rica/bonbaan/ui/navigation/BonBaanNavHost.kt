5ca646cfc71affe5c02ac3c8a94ae260a7573f29
FC4RICA <kantinan.fw@gmail.com>
2025-04-19 21:01:31 +0700

    Change: Modify start destination in BonBaanNavHost
    - Modified the start destination in `BonBaanNavHost.kt` to `Screen.Home.route` when the user is authenticated and has selected interests.

--- app/src/main/java/com/fc4rica/bonbaan/ui/navigation/BonBaanNavHost.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/navigation/BonBaanNavHost.kt
@@ -17,7 +17,7 @@ fun BonBaanNavHost(
     val startDestination = when {
         !isAuthenticated -> Screen.Auth.route
         !hasSelectedInterests -> Screen.Onboarding.route
-        else -> Screen.ServiceDetail.createRoute("")
+        else -> Screen.Home.route
     }
 
     NavHost(navController = navController, startDestination = startDestination) {
