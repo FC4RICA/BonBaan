5f09e3361d6d4df19b73caae1c4ea06d43ccd617
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-04-19 18:43:37 +0700

    Feat: Create ProfileScreen

--- app/src/main/java/com/fc4rica/bonbaan/ui/home/profile/ProfileScreen.kt
+++ app/src/main/java/com/fc4rica/bonbaan/ui/home/profile/ProfileScreen.kt
@@ -1,9 +1,201 @@
-package com.fc4rica.bonbaan.ui.home.profile
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.rounded.HourglassBottom
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import com.fc4rica.bonbaan.ui.MockScreen
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fc4rica.bonbaan.R
 
 @Composable
 fun ProfileScreen() {
-    MockScreen("PROFILE")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 30.dp)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+
+                    .size(40.dp)
+                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Text(
+                text = "บัญชี",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+
+        Card(shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Box(modifier = Modifier.size(60.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo1),
+                        contentDescription = "Profile",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+                            .align(Alignment.Center)
+                    )
+
+                    Icon(
+                        imageVector = Icons.Outlined.Edit,
+                        contentDescription = "Edit",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .align(Alignment.BottomEnd)
+                            .background(MaterialTheme.colorScheme.surface, CircleShape)
+                            .padding(2.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = "อาบิตคำ ชาตรี",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(29.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                "รายการสั่งซื้อ",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text("ดูคำสั่งซื้อทั้งหมด")
+        }
+
+
+        Spacer(modifier = Modifier.height(29.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            OrderItem(Icons.Outlined.ReceiptLong, "รอรับออเดอร์")
+            OrderItem(Icons.Outlined.AccountBalanceWallet, "ที่ต้องชำระ")
+            OrderItem(Icons.Rounded.HourglassBottom, "กำลังดำเนินการ")
+            OrderItem(Icons.Outlined.CheckCircle, "ที่ต้องยืนยัน")
+            OrderItem(Icons.Outlined.RateReview, "ที่ต้องรีวิว")
+        }
+
+        Spacer(modifier = Modifier.height(29.dp))
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "รายการรีวิวของฉัน",style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+            )
+            Icon(imageVector = Icons.Rounded.KeyboardArrowRight, contentDescription = "More")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "ช่วยเหลือ",style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,)
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                imageVector = Icons.Outlined.HelpOutline,
+                contentDescription = "Help"
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(17.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.QuestionAnswer,
+                contentDescription = "Chat",
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text("คุยกับเรา")
+        }
+    }
+}
+
+@Composable
+fun OrderItem(icon: ImageVector, label: String) {
+    Column(
+        modifier = Modifier.size(60.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(icon, contentDescription = label, modifier = Modifier.size(28.dp))
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = label,style = MaterialTheme.typography.labelSmall)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfileScreen() {
+    ProfileScreen()
 }
